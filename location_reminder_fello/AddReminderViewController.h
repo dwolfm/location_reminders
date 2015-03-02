d02678ad67ac07295d27098d6a0802dc3c56308c
Duncan Marsh <dwolfm@gmail.com>
2015-03-01 18:15:42 -0800

    main view buttons now pan map view to locations made one that gets a random location :)
    delight

---
+++ location_reminder_fello/AddReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddReminderViewController.h
+//  location_reminder_fello
+//
+//  Created by nacnud on 3/1/15.
+//  Copyright (c) 2015 nacnud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddReminderViewController : UIViewController
+
+@end
--- location_reminder_fello/ViewController.m
+++ location_reminder_fello/ViewController.m
@@ -8,10 +8,11 @@
 
 #import "ViewController.h"
 #import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController () <MKMapViewDelegate>
+@interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -20,6 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.mapView.delegate = self;
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
     
 }
 
@@ -34,14 +38,46 @@
 //MARK: handle button presses
 
 - (IBAction)homeButtonPressed:(UIButton *)sender {
+    CLLocationCoordinate2D homeCoordinate = CLLocationCoordinate2DMake(47.039199, -122.897258);
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.005, 0.005);
+    MKCoordinateRegion region = MKCoordinateRegionMake(homeCoordinate, span);
+    [self.mapView setRegion:region animated:true];
     
 }
 
+
+-(CLLocationDegrees) randomLat{
+    double rand = (double) arc4random();
+    double maxUint = (double) UINT_MAX;
+    double negOneToOne = ((rand/maxUint) * 2) -1;
+    double randomLat = negOneToOne*90;
+    NSLog(@"random latitude: %f", randomLat);
+    CLLocationDegrees latDegree = randomLat;
+    return latDegree;
+}
+
+-(CLLocationDegrees) randomLong{
+    double rand = (double) arc4random();
+    double maxUint = (double) UINT_MAX;
+    double negOneToOne = ((rand/maxUint) * 2) -1;
+    double randomLat = negOneToOne*180;
+    NSLog(@"random longitude: %f", randomLat);
+    CLLocationDegrees latDegree = randomLat;
+    return latDegree;
+}
+
 - (IBAction)randomButtonPressed:(UIButton *)sender {
+    CLLocationCoordinate2D cord = CLLocationCoordinate2DMake([self randomLat], [self randomLong]);
+    MKCoordinateSpan span = MKCoordinateSpanMake(10, 10);
+    MKCoordinateRegion region = MKCoordinateRegionMake(cord, span);
+    [self.mapView setRegion:region animated:true];
     
 }
 
 - (IBAction)codeFellowsButtonPressed:(UIButton *)sender {
-    
+    CLLocationCoordinate2D homeCoordinate = CLLocationCoordinate2DMake(47.623935, -122.335812);
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.005, 0.005);
+    MKCoordinateRegion region = MKCoordinateRegionMake(homeCoordinate, span);
+    [self.mapView setRegion:region animated:true];
 }
 @end
