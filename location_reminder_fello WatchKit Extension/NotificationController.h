73617c0f055fa32c58ac48102b4448f66eb3a3bf
Duncan Marsh <dwolfm@gmail.com>
2015-03-03 22:22:08 -0800

    switching to woking on delia

---
+++ location_reminder_fello WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  location_reminder_fello WatchKit Extension
+//
+//  Created by nacnud on 3/2/15.
+//  Copyright (c) 2015 nacnud. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
